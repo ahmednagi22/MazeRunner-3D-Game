01d2d022197a7df1497e956670082c627c680fbd
Ahmed1920-cell <70626826+Ahmed1920-cell@users.noreply.github.com>
2022-04-24 01:46:34 +0200

    Torch

--- Assets/Scripts C#/EnemyMovement.cs
+++ Assets/Scripts C#/EnemyMovement.cs
@@ -16,8 +16,17 @@ public class EnemyMovement : MonoBehaviour
     
     void Update()
     {
-
+    
     }
 
-  
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            Debug.Log("Do something here");
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+
+    }
 }
